bfd03bfd478c8c3718a51b8b7819d0f9ca1bef0d
Ami Duran <amiduran@live.com>
2022-06-01 11:25:18 -0500

    added donut code

--- foo.R
+++ foo.R
@@ -3,6 +3,7 @@ hungry <- readline(prompt = "are you hungry?")
 if (hungry=='yes') {
   print('eat a snack')
   print('eat a burger')
+  print('eat a donut')
 } else {
-  print('get back to work')
+  
 }
